9cf465a6b8d91d025cb37373a8b5a1cbe01ce57b
LuzM1243 <120821250+LuzM1243@users.noreply.github.com>
2023-04-20 18:47:31 -0500

    4/20/23

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 # reasearch on how to go to the next step.
 # created basic table and option
 # miro link for detailed progress updates https://miro.com/app/board/uXjVMV1Wqd0=/
+# 4/20/23 finally got the option 1 to my liking, but need to figure out how to get the meniu def to print out.
--- connect_db.py
+++ connect_db.py
@@ -17,59 +17,64 @@ if __name__ == "__main__":
     connection.cursor()
     cursor.execute("SELECT * FROM CUSTOMER")
     for x in cursor:
-        print('Showing info in table')
         print(x)
-balance="0"
-#user options
-#create account 
-#view balance 
-#remove account
-#exit
-print ("Select an option.\n 1. Create account\n 2. view balance\n 3. Remove account\n 4. exit program")
-option=input()
-# adding data into table(creating a new account)
-while True:
+        print('Showing info in table')
+
+def menu() : 
+    print ("Select an option.\n 1. Create account\n 2. view balance\n 3. Remove account\n 4. exit program")
+    option=input()
+    
+        # adding data into table(creating a new account)
+        #need a way to print out the main menu to test properly     
     if option == "1":
         try:
             user_idcustomer = int(input('Creating a new account*remember you account information*, Enter a id number.'))
-            
+                
         except ValueError:
             print('Please enter an integer.')
-            
-        #need a way to check if the id is not already in the data base
-        # firstname
+            return user_idcustomer
+            #need a way to check if the id is not already in the data base
+            # firstname
         try:
             user_firstname = (input('Input a firstname.'))
         except:    
             if not user_firstname.isalpha():
                 print("Only letters are allowed!")
-                
+                return user_firstname
         try:
             #lastname
             user_lastname = (input('Input a lastname.'))
         except:
             if not user_lastname.isalpha():
                 print("Only letters are allowed!")
+                return user_lastname
         try:
             user_pin = int(input('Enter a pin number.'))
-            print('verify this information')#need to print out user variables
+            verify = (input('verify this information:\n ID-{user_idcustomer}\n,Firstname-{user_firstname}\n Lastname-{user_lastname}\n Pin-{user_pin} (y/n)'))
+            #insert user option 1 inputs into table customers
+            if verify == ('y'):
+                connect_to_db()
+                user_option_1 = "INSERT INTO customers(idcustomers,firstname,lastname,pin,balance) VALUES (%s,%s,%s,%s,%s)"
+                op_1 = ({user_idcustomer},{user_firstname},{user_lastname},{user_pin},{balance})
+                cursor.execute(user_option_1, op_1)
+                for x in cursor:
+                    print('account created')
+                    print(x)
+                # see if the account was added
+                    if __name__ == "__main__":
+                        connect_to_db()
+                        connection = connect_to_db()
+                        cursor=connection.cursor()
+                        connection.cursor()
+                        cursor.execute("SELECT * FROM CUSTOMER")
+                        for x in cursor:
+                            print(x)
+                            print('Showing info in table')
+            return_to_menu = (input('return to menu? (y/n)'))
+            if return_to_menu == ("y"):
+                return menu
         except ValueError:
             print('Please enter an integer.')
-            #import option 1 data into customers table
-            
-    
-        
-    # print ('Creating a new account, Enter a firstname.')
-    # firstname = input()
-    # print('enter a lastname')
-    # lastname = input()
-    # print('Enter a pin number')
-    
-
-
-
-    # print ('Adding an account, to create a new account type in the info like so, ("firstname", "lastname", pin, balance)')
-    # query = "INSERT INTO customers(firstname, lastname, pin, balance) VALUES (%s,%s,%s,%s)"
-    # query_vals = input()
-    # #not working
-    # cursor.execute(query,query_vals)
+            return user_pin
+    if option == "4":
+        exit()
