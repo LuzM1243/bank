4be7114f344399ede475fda506ce26d9df1867b9
LuzM1243 <120821250+LuzM1243@users.noreply.github.com>
2023-04-23 22:23:21 -0500

    4/24/23

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 # reasearch on how to go to the next step.
 # created basic table and option
 # miro link for detailed progress updates https://miro.com/app/board/uXjVMV1Wqd0=/
-# 4/20/23 finally got the option 1 to my liking, but need to figure out how to get the meniu def to print out.
+# 4/20/23 finally got the option 1 to my liking, but need to figure out how to get the meniu def to print out
+# fixed last week's issu now i need to get theinputs to show in table
--- connect_db.py
+++ connect_db.py
@@ -22,6 +22,7 @@ if __name__ == "__main__":
         print('Showing info in table')
         # adding data into table(creating a new account)
         #need a way to print out the main menu to test properly 
+
 balance = 0
 running = True
 
@@ -30,56 +31,57 @@ option=input()
 
 def main_menu(options):
     print (options)
+def option_1 ():
+        id_q =print ('Creating a new account*remember you account information*, Enter a id number.')
+        user_idcustomer = input()
+        if user_idcustomer.isdigit():
+            user_firstname = (input('Input a firstname.'))
 
+            if user_firstname.isalpha():
+                user_lastname = (input('Input a lastname.')) 
 
-while running:
-    main_menu(options)    
-    if option == "1":
-            #id customer
-            user_idcustomer =(input('Creating a new account*remember you account information*, Enter a id number.'))
-            if user_idcustomer == int:
-                user_firstname = (input('Input a firstname.'))
-
-                if user_firstname.isalpha():
-                   user_lastname = (input('Input a lastname.')) 
-
-                   if user_lastname.isalpha():
-                      user_pin = (input('Enter a pin number.'))
-                      if user_pin == int:
-                        verify = (input('verify this information:\n ID-{user_idcustomer}\n,Firstname-{user_firstname}\n Lastname-{user_lastname}\n Pin-{user_pin} (y/n)'))
-                        #insert user option 1 inputs into table customers
+                if user_lastname.isalpha():
+                    user_pin = (input('Enter a pin number.'))
+                    if user_pin.isdigit():
+                        verify = (input(f'verify this information:\n ID-{user_idcustomer}\n Firstname-{user_firstname}\n Lastname-{user_lastname}\n Pin-{user_pin} (y/n)'))
+                    #insert user option 1 inputs into table customers
                         if verify == ('y'):
-                                    connect_to_db()
-                                    user_option_1 = "INSERT INTO customers(idcustomers,firstname,lastname,pin,balance) VALUES (%s,%s,%s,%s,%s)"
-                                    op_1 = ({user_idcustomer},{user_firstname},{user_lastname},{user_pin},{balance})
-                                    cursor.execute(user_option_1, op_1)
-                                    for x in cursor:
-                                        print('account created')
-                                        print(x)
-                                    # see if the account was added
-                                        if __name__ == "__main__":
-                                            connect_to_db()
-                                            connection = connect_to_db()
-                                            cursor=connection.cursor()
-                                            connection.cursor()
-                                            cursor.execute("SELECT * FROM CUSTOMER")
-                                            for x in cursor:
-                                                print(x)
-                                                print('Showing info in table')
-                      else:
-                          print('Please enter an integer.')
-                          #return user_pin   
-                   else:
-                       print("Letters only please.")
-                       #return user_firstname
-
+                                user_option_1 = "INSERT INTO customers(idcustomers,firstname,lastname,pin,balance) VALUES (%s,%s,%s,%s,%s)"
+                                op_1 = ({user_idcustomer},{user_firstname},{user_lastname},{user_pin},{balance})
+                                cursor.execute(user_option_1, op_1)
+                                connection.commit()
+                                print(cursor.rowcount,'account created')
+                                cursor.close()
+                                # see if the account was added
+                                if __name__ == "__main__":
+                                        connect_to_db()
+                                        cursor=connection.cursor()
+                                        connection.cursor()
+                                        cursor.execute("SELECT * FROM CUSTOMER")
+                                        for x in cursor:
+                                            print(x)
+                                            print('Showing info in table')
+                                            
+                        else:
+                            return id_q
+                    else:
+                        print('Please enter an integer.')
+                        return user_pin   
                 else:
                     print("Letters only please.")
-                    #return user_firstname
+                    return user_firstname
 
             else:
-                print('Please enter an integer.')
-                #return user_idcustomer
+                print("Letters only please.")
+                return user_firstname
 
+        else:
+            print('Please enter an integer.')
+            return user_idcustomer
+
+while running:
+    main_menu(options) 
+    option_1()
     if option =="4":
+        print ('exit')
         running = False
